098d4c7030073c406184a5f30341e04c1de04ac8
Ivan Berk <berkoivan@gmail.com>
2020-07-28 23:53:58 +0300

    Updates

---
+++ precache-manifest.023f3db0142b769b3dc8e7578268851e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5a634de9e559d2e245b5824bc154e9",
+    "url": "/ivanberko/App_React_JET-Solutions/index.html"
+  },
+  {
+    "revision": "25a4aebbbac00ab5e288",
+    "url": "/ivanberko/App_React_JET-Solutions/static/css/main.23b8aa6d.chunk.css"
+  },
+  {
+    "revision": "8e3143f87c5240dbac92",
+    "url": "/ivanberko/App_React_JET-Solutions/static/js/2.ed71608d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ivanberko/App_React_JET-Solutions/static/js/2.ed71608d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a4aebbbac00ab5e288",
+    "url": "/ivanberko/App_React_JET-Solutions/static/js/main.0ee53ce1.chunk.js"
+  },
+  {
+    "revision": "03e5fb256685b7efc8d7",
+    "url": "/ivanberko/App_React_JET-Solutions/static/js/runtime-main.44e4513d.js"
+  }
+]);
